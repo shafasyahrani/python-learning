044ba8a91b3b1e716163dff04cdbb7be74a9bdc9
Shafa Syahrani <shafa2294syahrani@gmail.com>
2025-12-26 22:07:40 +0700

    Add files via upload

---
+++ day1-variable.py
@@ -0,0 +1,30 @@
+#contoh output Python
+print("hello world!")
+
+#TIPE DATA DI PYTHON
+
+#Integer (int) : angka satuan yg bukan koma
+a = 6
+print((a), " ini adalah angka integer")
+print("tipe angka ini adalah ", type(a))
+
+#Float : angka koma
+b = 1.5
+print((b), " ini adalah angka float")
+print("tipe angka ini adalah ", type(b))
+
+#String : kumpulan karakter
+c = "Nama"
+print((c), " ini adalah angka String")
+print("tipe ini adalah ", type(c))
+
+#boolean : logika
+d = True
+print((d), " ini adalah angka boolean")
+print("tipe ini adalah ", type(d))
+
+#complex
+data_kompleks = complex(1,2)
+print((data_kompleks), " ini adalah angka complex")
+print("tipe angka ini adalah ", type(data_kompleks))
+
