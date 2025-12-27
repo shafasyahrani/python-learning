346a1095b626cf609129e6b26eff3bf84dc8ca37
Shafa Syahrani <shafa2294syahrani@gmail.com>
2025-12-27 20:26:10 +0700

    Add files via upload

---
+++ day2-operators.py
@@ -0,0 +1,62 @@
+#PYTHON OPERATORS
+print("====PYTHON OPERATORS====")
+
+#ARITHMETIC OPERATORS
+
+a = 10
+b = 2
+
+# addition (+)
+add = a + b
+print("hasil tambah:",add)
+
+#subtraction (-)
+sub = a - b
+print("hasil kurang:",sub)
+
+# multiplication (*)
+mul = a * b
+print("hasil kali:",mul)
+
+#division (/) | hasilnya float
+div = a / b
+print("hasil bagi:",div)
+
+#modulus (%)
+mod = a % b
+print("hasil modulus:",mod)
+
+#exponentation (**)/ pangkat
+exp = a ** b
+print("hasil pangkat:",exp)
+
+#floor division (//) | hasilnya integer
+fd = a // b
+print("hasil fd:",fd)
+
+#COMPARISON OPERATORS
+print("\n"+"="*4 + "COMPARISON OPERATORS" + "="*4)
+
+#Equal (==)
+eq = a == b
+print("hasil equal:",eq)
+
+#Not Equal (!=)
+ne = a != b
+print("hasil Not Equal:",ne)
+
+#Greater than (>)
+gt = a > b
+print("hasil Greater Than:",gt)
+
+#Less than (<)
+lt = a < b
+print("hasil Less than:",lt)
+
+#Greater than equal to (>=)
+x = a >= b
+print("hasil Greater than equal to:",x)
+
+#Less than equal to (<=)
+y = a <= b
+print("hasil Less than equal to:",y)
