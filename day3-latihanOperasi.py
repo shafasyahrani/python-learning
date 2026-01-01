d8d57cbe89d1160f56da61f86eb924da114d022b
Shafa Syahrani <shafa2294syahrani@gmail.com>
2026-01-01 20:42:22 +0700

    Add files via upload

---
+++ day3-latihanOperasi.py
@@ -0,0 +1,22 @@
+#latihan kalkulator sangat sederhana
+
+print("===KALKULATOR SEDERHANA===")
+
+a = float(input("Masukkan angka pertama: "))
+b = float(input("Masukkan angka kedua: "))
+
+#pertambahan
+tambah = a + b
+print("Hasil Tambah :", tambah)
+
+#Pengurangan
+kurang = a - b
+print("Hasil Kurang:", kurang)
+
+#Perkalian
+kali = a * b
+print("Hasil Kali :", kali)
+
+#Pembagian
+bagi = a / b
+print("Hasil Bagi: ", bagi)
