b1712b11edf7c9fd9eba9f39bd78b9cf6541ee71
Shafa Syahrani <shafa2294syahrani@gmail.com>
2026-01-04 21:32:21 +0700

    Add files via upload

---
+++ day5-operatorAssigment.py
@@ -0,0 +1,63 @@
+#OPERATOR ASSIGMENT
+#operator untuk menyingkat suatu operasi dengan assigment
+
+#(=)
+a = 4
+print("nilai a:", a)
+
+#(+=)
+a += 3 #artinya a = a + 3
+print("nilai a += 3:", a)
+
+#(-=)
+a -= 5 #artinya a = a - 5
+print("nilai a -= 5:",a)
+
+#(*=)
+a *= 10 #artinya a = a * 10
+print("nilai a *= 10:", a)
+
+#(/=)
+a /= 4 #artinya a = a / 4
+print("nilai a /= 4:", a)
+
+#(%=)
+a %= 2 #artinya a = a % 2
+print("nilai a %= 2:", a)
+
+#(//=) floor division
+a //= 1 #artinya a = a // 1
+print("nilai a //= 1:", a)
+
+#(**=) pangkat
+a **= 5 #artinya a = a ** 5
+print("nilai a **=:", a)
+
+
+#OPERASI BITWISE
+#AND (&=)
+x = True
+print("\nnilai x :", x)
+x &= False #artinya x = x and False
+print("nilai x &= False:", x)
+x &= True #artinya x = x & True
+print("nilai x &= True:", x)
+
+
+#OR (|=)
+y = False
+print("\nnilai y :", y)
+y |= True #artinya y = y or True
+print("nilai y |= True:", y)
+y |= False #artinya y = y or False
+print("nilai y |= False:", y)
+
+#XOR (^=)
+z = True
+print("\nnilai z:", z)
+z ^= True
+print("nilai z ^= True:", z)
+z = True
+z ^= False
+print("nilai z ^= False:", z)
+
