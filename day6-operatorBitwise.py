1adc8704cd5fcf876672588f7409b9bc25ea4b8f
Shafa Syahrani <shafa2294syahrani@gmail.com>
2026-01-05 21:12:05 +0700

    Add files via upload

---
+++ day6-operatorBitwise.py
@@ -0,0 +1,31 @@
+#OPERATOR BITWISE
+
+#digunakan untuk membandingkan nomor
+#AND OR XOR NOT Shiftleft ShiftRight
+
+a = 5
+b = 3
+
+#AND (&)
+c = a & b
+print("nilai c adalah:",c)
+
+#OR (|)
+c = a | b
+print("nilai c adalah:",c)
+
+#XOR (^)
+c = a ^ b
+print("nilai c adalah:",c)
+
+#NOT (~)
+c = ~a
+print("nilai c adalah:",c)
+
+#Shif Left (<<)
+c = a << b
+print("nilai c adalah:",c)
+
+#Shift Right (>>)
+c = a >> b
+print("nilai c adalah:", c)
