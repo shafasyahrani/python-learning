ee064f9e97cd5bbe5400c78a4542cec2f49e0e54
Shafa Syahrani <shafa2294syahrani@gmail.com>
2026-01-29 22:41:56 +0700

    Add files via upload

---
+++ day16-List.py
@@ -0,0 +1,31 @@
+#List
+#list digunakan untuk menyimpan banyak data item di satu variabel
+#list dapat dirubah (tambah, hapus, ganti), berurutan, dan bisa memiliki item yang sama dalam satu list
+
+#list data string
+color = ["brown","yellow","red"]
+print(color)
+
+#list data angka
+number = [12, 40, 13, 50]
+print(number)
+
+#list data Boolean
+pernyataan = [False, True, True]
+print(pernyataan)
+
+#list data campuran
+mix = ["Nama", 12, True]
+print(mix)
+
+#ingin mengetahui jumlah dalam list -> len()
+nama = ["Jane","Lisbon","Grace","Reid"]
+print(len(nama))
+
+#ingin mengetahui tipe data list -> type()
+food = ["Spaggeti","Burger","Pasta","Taco"]
+print(type(food))
+
+#cara alternatif membuat list
+angka = list((1,2,3,4,5))
+print(angka)
