2abfeb57f3b2dd931f66f1f481616fecc890b3e9
Shafa Syahrani <shafa2294syahrani@gmail.com>
2026-02-02 22:37:07 +0700

    Add files via upload

---
+++ day17-List_Change.py
@@ -0,0 +1,18 @@
+#Change List Items 
+mylist = ["sepatu","tas","topi","kacamata"]
+mylist[3] = "bando" #item index ke 3 diganti menjadi "bando"
+print(mylist)
+
+mylist = ["makan","belajar","nyanyi","baca","nulis"]
+mylist[2:4] = ["tidur","masak"] #item index ke 2 dan 3 diganti menjadi "tidur" dan "masak"
+print(mylist)
+
+mylist = ["biru","kuning","pink"]
+mylist[1:2] = ["hijau","merah"] #item index ke 1 "kuning" diganti menjadi "hijau" dan "merah"
+print(mylist)
+
+#Insert Items (menambahkan item berdasarkan spesifik index)
+mylist = ["apel","mangga","melon"]
+mylist.insert(2,"semangka") #menambahkan item "semangka" pada index ke 2 
+print(mylist)
+
