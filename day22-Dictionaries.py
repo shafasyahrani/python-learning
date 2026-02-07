b29360cc5c60320f03f16bcf776708e4b0caedc2
Shafa Syahrani <shafa2294syahrani@gmail.com>
2026-02-07 20:44:15 +0700

    Add files via upload

---
+++ day22-Dictionaries.py
@@ -0,0 +1,14 @@
+#Dictionaries
+#dictionaries kumpulan data yang berurutan, dapat dirubah, dan tidak dapat memiliki item yang sama
+
+thisdict = {
+    "Nama" : "Shafa",
+    "Umur" : 22,
+    "jurursan" : "sistem informasi"
+}
+print(thisdict)
+
+thisdict = dict(Nama="Sheila",Umur=21,jurusan="sistem informasi")
+print(thisdict)
+
+
